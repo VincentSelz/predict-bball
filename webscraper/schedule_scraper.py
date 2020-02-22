ab244383d8e1c26f16291028aa39e18f686c88ec
Vincent Selz <s6viselz@uni-bonn.de>
2020-02-22 14:16:31 +0100

    Write code as function.

--- webscraper/schedule_scraper.py
+++ webscraper/schedule_scraper.py
@@ -6,48 +6,69 @@ from urllib.error import HTTPError
 from urllib.request import urlopen
 
 import pandas as pd
-import requests
 from bs4 import BeautifulSoup
 
+# import requests
+
 # NBA season that are analyzed
-months = [
-    "october",
-    "november",
-    "december",
-    "january",
-    "february",
-    "march",
-    "april",
-    "may",
-    "june",
-]
+years = [2010, 2011, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
-box_scores = []
-for month in months:
-    try:
-        url = f"""https://www.basketball-reference.com/leagues/NBA_2012_games-{month}.html"""
 
-        # Is the URL accessible?
-        response = requests.get(url)
-        # this is the HTML from the given URL
-        html = urlopen(url)
-        soup = BeautifulSoup(html)
-    except HTTPError as err:
-        if err.code == 404:
-            continue
-        else:
-            raise HTTPError
-    # use findALL() to get the column headers
-    soup.findAll("tr", limit=2)
+def schedule_scraper(years):
+    """
+    This function takes a list of years as argument and scrapes the site for these years and
+    returns the NBA schedule as Excel-document.
+    Args: list of years
+    """
+    months = [
+        "october",
+        "november",
+        "december",
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+    ]
 
-    # use getText()to extract the text we need into a list
-    headers = [th.getText() for th in soup.findAll("tr", limit=2)[0].findAll("th")]
+    box_scores = {}
+    for year in years:
+        box_scores[year] = []
+        for month in months:
+            try:
+                url = (
+                    f"https://www.basketball-reference.com/"
+                    "leagues/NBA_{year}_games-{month}.html"
+                )
+                # Is the URL accessible?
+                # response = requests.get(url)
+                # this is the HTML from the given URL
+                html = urlopen(url)
+                soup = BeautifulSoup(html)
+            except HTTPError as err:
+                if err.code == 404:
+                    continue
+                else:
+                    raise HTTPError  # TODO: FInd better Error + Message
+            # use findALL() to get the column headers
+            soup.findAll("tr", limit=2)
 
-    # avoid the first header row
-    rows = soup.findAll("tr")[1:]
-    games = [[td.getText() for td in rows[i]] for i in range(len(rows))]
+            # use getText()to extract the text we need into a list
+            headers = [
+                th.getText() for th in soup.findAll("tr", limit=2)[0].findAll("th")
+            ]
 
-    # Convert into Dataframe
-    month = pd.DataFrame(games, columns=headers)
-    box_scores.append(month)
-box_scores = pd.concat(box_scores)
+            # avoid the first header row
+            rows = soup.findAll("tr")[1:]
+            games = [[td.getText() for td in rows[i]] for i in range(len(rows))]
+
+            # Convert into Dataframe
+            month = pd.DataFrame(games, columns=headers)
+            box_scores[year].append(month)
+
+        # Create a Dataframe for each year
+        box_scores[year] = pd.concat(box_scores[year])
+
+        # Export it as Excel file
+        # box_scores[year].to_excel("../Data/Boxscores"+ str(year) +".xlsx")
