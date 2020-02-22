d82b3c720b6c8af5d04b1fe21d7f910545651b85
Vincent Selz <s6viselz@uni-bonn.de>
2020-02-22 13:31:11 +0100

    Make the scraper work for one year of data.

--- webscraper/schedule_scraper.py
+++ webscraper/schedule_scraper.py
@@ -8,27 +8,30 @@ import pandas as pd
 import requests
 import time
 
+# NBA season that are analyzed
+months = ["october", "november", "december", "january", "february", "march", "april", "may", "june"]
+
 box_scores = []
+for month in months:
+    url = """https://www.basketball-reference.com/leagues/NBA_2019_games-{}.html""".format(month)
 
-url = """https://www.basketball-reference.com/leagues/NBA_2019_games-october.html"""
+    # Is the URL accessible?
+    response = requests.get(url)
+    # this is the HTML from the given URL
+    html = urlopen(url)
+    soup = BeautifulSoup(html)
+    # use findALL() to get the column headers
+    soup.findAll('tr', limit=2)
 
-# Is the URL accessible?
-response = requests.get(url)
-# this is the HTML from the given URL
-html = urlopen(url)
-soup = BeautifulSoup(html)
-# use findALL() to get the column headers
-soup.findAll('tr', limit=2)
+    # use getText()to extract the text we need into a list
+    headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
 
-# use getText()to extract the text we need into a list
-headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-
-# avoid the first header row
-rows = soup.findAll('tr')[1:]
-games = [[td.getText() for td in rows[i]]
+    # avoid the first header row
+    rows = soup.findAll('tr')[1:]
+    games = [[td.getText() for td in rows[i]]
             for i in range(len(rows))]
 
-# Convert into Dataframe
-month = pd.DataFrame(games, columns = headers)
-box_scores.append(month)
+    # Convert into Dataframe
+    month = pd.DataFrame(games, columns = headers)
+    box_scores.append(month)
 box_scores = pd.concat(box_scores)
