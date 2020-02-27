23f90033b88b5b6ddc599063d6311a7490cfd3bb
Vincent Selz <s6viselz@uni-bonn.de>
2020-02-27 16:02:34 +0100

    Make it work for all years.

--- webscraper/schedule_scraper.py
+++ webscraper/schedule_scraper.py
@@ -1,22 +1,21 @@
 """This program scrapes basketball-reference.com to attain the scores
 for each game in the specified season and exports them as excel files.
 """
-# import time
+import time
 from urllib.error import HTTPError
 from urllib.request import urlopen
 
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
 # NBA season that are analyzed
-years = [2010, 2011, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+# years = [2010, 2011, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
 
 def schedule_scraper(years):
     """
     This function takes a list of years as argument and scrapes the site for these years and
-    returns the NBA schedule as Excel-document.
+    returns the NBA schedules as Excel-document.
     Args: list of years
     """
     months = [
@@ -38,17 +37,19 @@ def schedule_scraper(years):
             try:
                 url = (
                     f"https://www.basketball-reference.com/"
-                    "leagues/NBA_{year}_games-{month}.html"
+                    "leagues/NBA_{}_games-{}.html".format(year, month)
                 )
                 # this is the HTML from the given URL
                 html = urlopen(url)
-                soup = BeautifulSoup(html)
             except HTTPError as err:
                 if err.code == 404:
                     continue
                 else:
                     raise HTTPError("Request was not successful.")
-
+            # except HTTPError:
+            #    continue
+            # make soup
+            soup = BeautifulSoup(html)
             # use findALL() to get the column headers
             soup.findAll("tr", limit=2)
 
@@ -65,8 +66,14 @@ def schedule_scraper(years):
             month = pd.DataFrame(games, columns=headers)
             box_scores[year].append(month)
 
+            # As specified in the robots.txt of the website.
+            time.sleep(3)
+
         # Create a Dataframe for each year
         box_scores[year] = pd.concat(box_scores[year])
-
         # Export it as Excel file
-        # box_scores[year].to_excel("../Data/Boxscores"+ str(year) +".xlsx")
+        box_scores[year].to_excel("../Data/Boxscores" + str(year) + ".xlsx")
+
+
+list_of_years = list(range(2010, 2019))
+schedule_scraper(list_of_years)
