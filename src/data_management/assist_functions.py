84e54b94cce9f9bfdbbfa377a33f8d3289e02ce7
Vincent Selz <s6viselz@uni-bonn.de>
2020-03-07 12:07:26 +0100

    Correct spelling error.

--- src/data_management/assist_functions.py
+++ src/data_management/assist_functions.py
@@ -79,7 +79,7 @@ def clean_base_function(year):
     ]
 
     # dc = pd.read_csv(f"../data/Boxscores{year}.csv", parse_dates=True)
-    dc = pd.read_csv(ppj("IN_DATA", f"Boxscores{year}.excel"))
+    dc = pd.read_csv(ppj("IN_DATA", f"Boxscores{year}.xlsx"))
 
     # List of new columns names
     dc.columns = headers
